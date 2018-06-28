9f6a6dbb6c7fb7264eb3c39efd54191086c53992
Nctomt <625216344@qq.com>
2018-06-28 21:06:26 +0800

    2nd time

--- TextQuery.cc
+++ TextQuery.cc
@@ -35,26 +35,28 @@ public:
 	QueryResult query(const string&) const;
 
 private:
-	shared_ptr<vector<string>> file;
-	map<string, shared_ptr<set<line_no>>> wm;
+	shared_ptr<vector<string>> file;			//输入文件
+	map<string, shared_ptr<set<line_no>>> wm;	//每个单词到所在行号集合的映射
 };
 
-TextQuery::TextQuery(ifstream & is)
+//TextQuery构造函数：接收一个ifstream 逐行读取文件
+TextQuery::TextQuery(ifstream & is)			
 : file(new vector<string>)
 {	
 	string text;
-	while(getline(is, text))
+	while(getline(is, text))//循环对文件的每一行进行操作
 	{
-		file->push_back(text);
-		int n = file->size() - 1;
-		istringstream line(text);
-		string word;
-		while(line >> word)
+		file->push_back(text);		//保存文本
+		int n = file->size() - 1;	//行号
+		istringstream line(text);	//文本分解为单词
+		string word;				
+		while(line >> word)//循环对此行中每一个单词操作
 		{
-			auto & lines = wm[word];
-			if(!lines)
-				lines.reset(new set<line_no>);
-			lines->insert(n);
+			//如果单词不再wm中  把它作为下标 添加到wm中 
+			auto & lines = wm[word];//lines是一个shared_ptr
+			if(!lines)				//第一次出现某个单词 指针为空
+				lines.reset(new set<line_no>);//分配新set
+			lines->insert(n);		//将行号插入到set中
 		}
 	}
 }
@@ -64,23 +66,25 @@ class QueryResult
 	friend std::ostream & print(std::ostream &, const QueryResult &);
 public:
 	using line_no = vector<string>::size_type;
-	QueryResult(string s, shared_ptr<set<line_no>> p, 
-			shared_ptr<vector<string>> f)
+	QueryResult(string s,
+				shared_ptr<set<line_no>> p,
+				shared_ptr<vector<string>> f)
+	//构造函数：string保存查询单词 -- s   
+	//shared_ptr保存单词出现行号的set -- p   
+	//shared_ptr指向保存输入文件的vector -- f
 	: sought(s)
 	, lines(p)
 	, file(f)
 	{}
 	
 	set<line_no>::const_iterator begin()
-	{	return lines->begin();	}
+	{	return lines->begin();		}
 
 	set<line_no>::const_iterator end()
 	{	return lines->end();		}
 
 	shared_ptr<vector<string>> get_file()
-	{	return file;			}
-
-
+	{	return file;				}
 
 private:
 	string sought;
@@ -88,30 +92,34 @@ private:
 	shared_ptr<vector<string>> file;
 };
 
+//接受一个string  查询单词
 QueryResult TextQuery::query(const string & sought) const
 {
+	//未找到string（查询单词） 返回一个指向此的set指针
 	static shared_ptr<set<line_no>> nodata(new set<line_no>);
+	//使用find而不是下标 ：避免将单词添加到wm中
 	auto loc = wm.find(sought);
-	if(loc == wm.end())
+	if(loc == wm.end())//未找到
 		return QueryResult(sought, nodata, file);
 	else
 		return QueryResult(sought, loc->second, file);
 }
-
+//打印结果:在给定的流上打印出给定的QueryResult对象
 std::ostream & print(std::ostream & os, const QueryResult & qr)
 {
-	os << qr.sought << "occur" << qr.lines->size()
-	   << " " << (qr.lines->size() > 1 ? "times" : "time" ) << endl;
-	for(auto num : * qr.lines)
+	//打印所以出现次数 以及行号
+	os << qr.sought << " occurs " << qr.lines->size()
+	   << " " << (qr.lines->size() > 1 ? "times:" : "time:" ) << endl;
+	for(auto num : * qr.lines)	//对set中的每一个单词
 	{
-		os << "\t(line " << num + 1 << ")" 
+		os << " (line " << num + 1 << ")" 
 		   << *(qr.file->begin() + num) << endl;
 	}
-
 	return os;
-
 }
 
+
+//功能2.0 ：在查询单词的基础上，增加了与或非查询功能
 class Query_base
 {//查询基类  具体的查询类型从中派生。所有的成员都是private的
 	friend class Query;
@@ -128,13 +136,13 @@ private:
 };
 
 class Query
-{	//管理Query继承体系的类		
+{	//管理Query继承体系的类   是Query_base的唯一接口		
 	friend Query operator~(const Query &);
 	friend Query operator|(const Query &, const Query &);
 	friend Query operator&(const Query &, const Query &);
 
 public:
-	Query(const string &);
+	Query(const string &);			//构建一个新的wordQuery
 
 	//接口对应Query_base 操作
 	QueryResult eval(const TextQuery &t) const
@@ -144,6 +152,8 @@ public:
 	{	 return q->rep();		}
 
 private:
+	//不希望一般用户能随便定义Query_base对象
+	//所以讲三个运算符声明为友元
 	Query(shared_ptr<Query_base> query)
 	: q(query)
 	{}
@@ -154,6 +164,8 @@ private:
 std::ostream & operator<< (std::ostream & os, const Query & query)
 {	return os << query.rep();	}
 
+
+//保存要查找的单词
 class WordQuery
 : public Query_base
 {
@@ -169,9 +181,10 @@ class WordQuery
 	string rep() const
 	{	return query_word;		}
 
-	string query_word;
+	string query_word;			//要查找的单词
 };
-
+//定义了WordQuery类 就能定义接收string的Query构造函数了
+//构造函数分配一个WordQuery 然后领指针成员指向新分配的对象
 inline
 Query::Query(const string & s)
 : q(new WordQuery(s)) {}
@@ -193,11 +206,15 @@ class NotQuery
 	QueryResult eval(const TextQuery &) const;
 	Query query;
 };
-
+//将得到的NotQuery指针绑定到一个shared_ptr<Query_base>
 inline
 Query operator~(const  Query & operand)
 {	return shared_ptr<Query_base>(new NotQuery(operand));	}
 
+
+
+//抽象基类 保存操作两个运算对象的查询类型所需数据
+//用于| 和 &
 class BinaryQuery
 : public Query_base
 {
@@ -211,10 +228,9 @@ protected:
 	string rep() const 
 	{	return "(" + lhs.rep() + " " + opSym + " " + rhs.rep() + ")";	}
 
-	Query lhs, rhs;
-	string opSym;
+	Query lhs, rhs;		//左右测运算对象
+	string opSym;		//运算符名字
 };
-
 
 
 class AndQuery
@@ -228,7 +244,6 @@ class AndQuery
 
 	QueryResult eval(const TextQuery &) const;
 };
-
 inline
 Query operator &(const Query & lhs, const Query & rhs)
 {	return shared_ptr<Query_base>(new AndQuery(lhs, rhs));	}
@@ -245,42 +260,56 @@ class OrQuery
 
 	QueryResult eval(const TextQuery &) const;
 };
-
 inline Query operator|(const Query & lhs, const Query & rhs)
 {	return shared_ptr<Query_base>(new OrQuery(lhs, rhs));	}
 
+
+
 QueryResult OrQuery::eval(const TextQuery & text) const
 {
+	//通过lhs  rhs 进行虚调用
 	auto right = rhs.eval(text), left = lhs.eval(text);
+	//将左侧运算对象的结果  拷贝到结果set中
 	auto ret_lines = make_shared<set<line_no>>(left.begin(), left.end());
+	//插入右侧运算对象所得行号
 	ret_lines->insert(right.begin(), right.end());
+	//返回rhs 和 lhs 并集
 	return QueryResult(rep(), ret_lines, left.get_file());
+	//返回值：表示查询的string   
+	//		  匹配行号set的shared_ptr
+	//		  指向输入文件的vector的shared_ptr
 }
 
 QueryResult AndQuery::eval(const TextQuery & text) const
-{
+{	//和Or的差别是  调用了标准库算法来查询结果的交集（行）
 	auto left = lhs.eval(text), right = rhs.eval(text);
+	//标准库算法 取交集
 	auto ret_lines = make_shared<set<line_no>>();
-	set_intersection(left.begin(), left.end(), right.begin(),right.end(), inserter(*ret_lines, ret_lines->begin()));
+	//将两个范围的交集写入一个目标迭代器中
+	//标准库算法 set_intersection合并两个set
+	set_intersection(left.begin(), left.end(),
+					 right.begin(),right.end(), 
+					 inserter(*ret_lines, ret_lines->begin()));
 	return QueryResult(rep(), ret_lines, left.get_file());
 }
 
 QueryResult NotQuery::eval(const TextQuery & text) const
 {
+	//通过Query运算对象对eval进行虚调用
 	auto result = query.eval(text);
 	auto ret_lines = make_shared<set<line_no>>();
-	auto beg = result.begin(), end = result.end();
+	auto beg = result.begin(), end = result.end();//必须对所有行进行迭代
+	//对输入文件每一行， 如果不在result中  添加到ret_lines中
 	auto sz = result.get_file()->size();
 	for(size_t n = 0; n != sz; ++n)
 	{
 		if(beg == end || *beg != n)
-			ret_lines->insert(n);
+			ret_lines->insert(n);				 //如果不存在 添加到此
 		else if(beg != end)
-			++beg;
+			++beg;								 //存在 获取下一行
 	}
 	return QueryResult(rep(), ret_lines, result.get_file());
 }
-
 
 
 int main(int argc, char *argv[])
@@ -290,9 +319,26 @@ int main(int argc, char *argv[])
 		cout << "error args!" << endl;
 		return 0;
 	}
-
 	ifstream ifs(argv[1]);
+
 	TextQuery tq(ifs);
+
+	Query q1 = Query("Daddy");
+	print(cout, q1.eval(tq));
+
+	cout << endl;
+	Query q2 = ~Query("Alice");
+	print(cout, q2.eval(tq));
+
+	cout << endl;
+	Query q3 = Query("hair") | Query("Alice");
+	print(cout, q3.eval(tq));
+
+	cout << endl;
+	Query q4 = Query("hair") & Query("Alice");
+	print(cout, q4.eval(tq));
+
+	cout << endl;
 	Query q = Query("fiery") & Query("bird") | Query("wind");
 	print(cout, q.eval(tq));
 	
